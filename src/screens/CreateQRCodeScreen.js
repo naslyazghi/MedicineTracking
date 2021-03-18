195d6f98a38977f3317f79ac64844862a0619d0b
Tara Rutkowski <tararutkowski@gmail.com>
2021-03-18 13:38:03 -0400

    dropdown and print?? in qr code creator

--- src/screens/CreateQRCodeScreen.js
+++ src/screens/CreateQRCodeScreen.js
@@ -1,57 +1,112 @@
 'use strict';
  
 import React, { Component } from 'react'
+import {ScrollView, Text} from 'react-native';
+import {FilledButton} from '../components/FilledButton';
 import QRCode from 'react-native-qrcode-svg';
-
-
 import {
     AppRegistry,
     StyleSheet,
     View,
     TextInput
 } from 'react-native';
- 
+import DropDownPicker from 'react-native-dropdown-picker';
+
 class QRScreen extends Component {
+
   state = {
-    text: 'https://medium.com/@mushtaque87',
+    type: 'order',
+    //can you chnage num to say order number if selected order
+    //and NDC if selected product?
+    num: ''
   };
- 
+  getDataURL() {
+    this.svg.toDataURL(this.callback);
+  }
+  
+  callback(dataURL) {
+    console.log(dataURL);
+  }
+
   render() {
+
     return (
+      <ScrollView>
       <View style={styles.container}>
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => this.setState({text: text})}
-          value={this.state.text}
-        />
-        {/* <QRCode
-      value={this.state.text}
-    /> */}
+      <Text style={styles.heading}>Add Info for QR Code</Text>
+
+        <View style={styles.action}>
+
+          <DropDownPicker
+              items={[
+                  {label: 'Order in Transit', value: 'order'},
+                  {label: 'Product at Base', value: 'product'}
+              ]}
+              defaultValue={this.state.type}
+              containerStyle={{height: 40}}
+              style={{backgroundColor: '#fafafa'}}
+              itemStyle={{
+                  justifyContent: 'flex-start'
+              }}
+              dropDownStyle={{backgroundColor: '#fafafa'}}
+              onChangeItem={item => this.setState({
+                  type: item.value,
+              })}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder = "Insert Order Number or Product NDC"
+            onChangeText={(num) => this.setState({num: num})}
+            value={this.state.num}
+          />
+        </View>
+
+        <Text style={styles.heading}>Generated QR Code</Text>
         <QRCode
-          value={this.state.text.length > 0 ? this.state.text : "health app"}
+          value={this.state.type.length > 0 ? JSON.stringify(this.state) : "improper QR"}
           size={200}
           bgColor='#000000'
-          fgColor='#FFFFFF'/>
+          fgColor='#FFFFFF'
+          getRef={(c) => (this.svg = c)}
+        />
+        <FilledButton
+          title={'Print'}
+          style={styles.loginButton}
+         // onPress={() => {
+           //   print();
+         // }}
+        />
       </View>
+      </ScrollView>
     );
   };
 }
  
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         backgroundColor: 'white',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
  
     input: {
-        height: 40,
+        flex: 0,
         borderColor: 'gray',
         borderWidth: 1,
         margin: 10,
         borderRadius: 5,
         padding: 5,
+    },
+
+    heading: {
+      fontSize: 18,
+      marginTop: 30,
+      marginBottom: 5,
+      backgroundColor: '#74848f',
+      width: '100%',
+      paddingHorizontal: 10,
+      paddingVertical: 4,
+      color: 'white',
     }
 });
  
