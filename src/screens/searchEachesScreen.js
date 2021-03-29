542b8159fd3acafcf48ae8974faa0b81845bd3fd
Tara Rutkowski <tararutkowski@gmail.com>
2021-03-29 17:27:40 -0400

    added cool search bar from tutorial, trying to update for app but cant get fetch to work

--- src/screens/searchEachesScreen.js
+++ src/screens/searchEachesScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+/*import React, { useState, useEffect } from 'react';
 import { View, Text, Button, TextInput, TouchableOpacity, Dimensions, StyleSheet, StatusBar, Image } from 'react-native';
 
 
@@ -35,7 +35,7 @@ const searchEachesScreen = ({navigation}) => {
         />
         <Button 
         title={'Tap to Search'} 
-        /*onPress={navigation.navigate("SearchEachesResults", {ndc, propname, labeler})} */
+        /*onPress={navigation.navigate("SearchEachesResults", {ndc, propname, labeler})} 
         />
       </View>  
 
@@ -74,4 +74,116 @@ const styles = StyleSheet.create({
 
 export default searchEachesScreen;
 
+*/
 
+  
+import React, { Component } from 'react';
+import { View, Text, FlatList, ActivityIndicator } from 'react-native';
+import { ListItem, SearchBar } from 'react-native-elements';
+
+class searchEachesScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      data: [],
+      error: null,
+    };
+
+    this.arrayholder = [];
+  }
+
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+  makeRemoteRequest = () => {
+    const url = `https://randomuser.me/api/?&results=20`;
+    this.setState({ loading: true });
+
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          data: res.results,
+          error: res.error || null,
+          loading: false,
+        });
+        this.arrayholder = res.results;
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+      });
+  };
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: '86%',
+          backgroundColor: '#CED0CE',
+          marginLeft: '14%',
+        }}
+      />
+    );
+  };
+
+  searchFilterFunction = text => {
+    this.setState({
+      value: text,
+    });
+
+    const newData = this.arrayholder.filter(item => {
+      const itemData = `${item.name.title.toUpperCase()} ${item.name.first.toUpperCase()} ${item.name.last.toUpperCase()}`;
+      const textData = text.toUpperCase();
+
+      return itemData.indexOf(textData) > -1;
+    });
+    this.setState({
+      data: newData,
+    });
+  };
+
+  renderHeader = () => {
+    return (
+      <SearchBar
+        placeholder="Type Here..."
+        lightTheme
+        round
+        onChangeText={text => this.searchFilterFunction(text)}
+        autoCorrect={false}
+        value={this.state.value}
+      />
+    );
+  };
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+    return (
+      <View style={{ flex: 1 }}>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <ListItem
+              leftAvatar={{ source: { uri: item.picture.thumbnail } }}
+              title={`${item.name.first} ${item.name.last}`}
+              subtitle={item.email}
+            />
+          )}
+          keyExtractor={item => item.email}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+        />
+      </View>
+    );
+  }
+}
+
+export default searchEachesScreen;
